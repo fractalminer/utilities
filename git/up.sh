9a4cd060450c5535c50b6f45264d51e1b337baf9
David P. Sicilia <dpsicilia@gmail.com>
2026-08-23 10:52:02 -0400

    Update up with redist

--- git/up.sh
+++ git/up.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-pull() {
-  local where="$1"
-  local branch="$2"
+pull_impl() {
+  local branch="$1"
   cd "$where"
-  echo -en "$(basename "$where"):\t"
   git fetch origin "$branch" --quiet
   # If there are conflicts then we want it to fail and not mess
   # things up, so just disable the auto-stash, otherwise in some
@@ -13,6 +11,22 @@ pull() {
   git merge "origin/$branch" --no-autostash
 }
 
-pull ~/dev/utilities master
-pull ~/dev/moonlib   main
-pull ~/.dotfiles     master
+pull() {
+  local where="$1"
+  local branch="$2"
+  echo -en "$(basename "$where"):\t"
+  pull_impl "$branch"
+}
+
+# For shorter names that need two tabs.
+pull2() {
+  local where="$1"
+  local branch="$2"
+  echo -en "$(basename "$where"):\t\t"
+  pull_impl "$branch"
+}
+
+pull  ~/dev/utilities master
+pull  ~/dev/moonlib   main
+pull  ~/.dotfiles     master
+pull2 ~/dev/redist    main
