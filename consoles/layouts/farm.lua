72965ccd2226c8a4f103c46556e9a9094db97227
David P. Sicilia <dpsicilia@gmail.com>
2026-09-02 10:16:48 -0400

    Clean up farm scripts.

---
+++ consoles/layouts/farm.lua
@@ -0,0 +1,28 @@
+local function WORKER( host )
+  return command{ 'ssh ' .. host .. ' -t "tmux new -s farm -A"' }
+end
+
+local NONE   = command{ 'echo none' }
+
+return vertical{
+  [1]=horizontal{
+    [1]=vertical{
+      [1]=WORKER( 'bonobo' ),
+      [2]=WORKER( 'thelio' ),
+    },
+    [2]=vertical{
+      [1]=WORKER( 'meerkat' ),
+      [2]=WORKER( 'geekom1' ),
+    },
+  },
+  [2]=horizontal{
+    [1]=vertical{
+      [1]=WORKER( 'geekom2' ),
+      [2]=WORKER( 'darter2' ),
+    },
+    [2]=vertical{
+      [1]=NONE,
+      [2]=NONE,
+    },
+  },
+}
