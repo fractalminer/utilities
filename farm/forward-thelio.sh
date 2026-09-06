b1adc65791d3fcc1e5067d41d08edd1f8822ecc6
David P. Sicilia <dpsicilia@gmail.com>
2026-09-05 21:00:39 -0400

    Update forward-thelio

--- farm/forward-thelio.sh
+++ farm/forward-thelio.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eo pipefail
-set -x
 
 # redis linode -> redis thelio.
 # 12345 linode -> ssh thelio
@@ -10,6 +9,14 @@ set -x
 # previous sessions that are still bound to those ports, which
 # otherwise would just allow the ssh but silently not map the
 # ports.
-ssh -N -o ExitOnForwardFailure=yes linode \
-  -R 127.0.0.1:6379:localhost:6379 \
-  -R 127.0.0.1:12345:localhost:22
+while true; do
+  echo "... linode redis <-- thelio redis ..."
+  echo "... linode ssh   <-- thelio ssh   ..."
+  # NOTE: we don't want a failure on this command to terminate
+  # the script.
+  ssh -N -o ExitOnForwardFailure=yes linode \
+    -R 127.0.0.1:6379:localhost:6379 \
+    -R 127.0.0.1:12345:localhost:22 \
+    || true
+  sleep 10
+done
